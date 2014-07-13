fa3e170aadc227e9c6b38f5c3aef633d2623e5e1
Mehmet Ataş <mehmet@mehmetatas.net>
2014-07-13 17:52:59 +0300

    remove facebook

--- ballgame/src/com/turpgames/ballgame/components/ResultView.java
+++ ballgame/src/com/turpgames/ballgame/components/ResultView.java
@@ -1,13 +1,10 @@
 package com.turpgames.ballgame.components;
 
 import com.turpgames.ballgame.utils.R;
-import com.turpgames.entity.Player;
 import com.turpgames.framework.v0.IDrawable;
-import com.turpgames.framework.v0.client.TurpClient;
 import com.turpgames.framework.v0.component.IButtonListener;
 import com.turpgames.framework.v0.component.TextButton;
 import com.turpgames.framework.v0.impl.ScreenManager;
-import com.turpgames.framework.v0.social.ICallback;
 import com.turpgames.framework.v0.util.Color;
 import com.turpgames.framework.v0.util.Game;
 
@@ -18,52 +15,53 @@ public class ResultView implements IDrawable {
 		public abstract void onShareScore();
 	}
 
-	private final static String loginWithFacebook = "Login with Facebook";
-	private final static String shareScore = "Share Score";
+	// private final static String loginWithFacebook = "Login with Facebook";
+	// private final static String shareScore = "Share Score";
 
 	private final TextButton restartButton;
-	private final TextButton shareScoreButton;
+	// private final TextButton facebookButton;
 	private final TextButton aboutButton;
 	private final TextButton hiScoresButton;
 	private final BallGameLogo logo;
 
-	private boolean hideShareScoreButton;
+	// private boolean hideShareScoreButton;
 
 	public ResultView(final IListener listener) {
 		float y = Game.getVirtualHeight() / 2.0F;
-		shareScoreButton = createButton(shareScore, y + 90f, new IButtonListener() {
-			public void onButtonTapped() {
-				if (shareScoreButton.getText().equals(shareScore))
-					listener.onShareScore();
-				else
-					TurpClient.loginWithFacebook(new ICallback() {
-						@Override
-						public void onSuccess() {
-							shareScoreButton.setText(shareScore);
-							setLocation(shareScoreButton, shareScoreButton.getLocation().y);
-						}
+		// facebookButton = createButton(shareScore, y + 90f, new
+		// IButtonListener() {
+		// public void onButtonTapped() {
+		// if (facebookButton.getText().equals(shareScore))
+		// listener.onShareScore();
+		// else
+		// TurpClient.loginWithFacebook(new ICallback() {
+		// @Override
+		// public void onSuccess() {
+		// facebookButton.setText(shareScore);
+		// setLocation(facebookButton, facebookButton.getLocation().y);
+		// }
+		//
+		// @Override
+		// public void onFail(Throwable t) {
+		//
+		// }
+		// });
+		// }
+		// });
 
-						@Override
-						public void onFail(Throwable t) {
-							
-						}
-					});
-			}
-		});
-
-		restartButton = createButton("Play Again", y - 180f, new IButtonListener() {
+		restartButton = createButton("Play Again", y - 150f, new IButtonListener() {
 			public void onButtonTapped() {
 				listener.onRestartGame();
 			}
 		});
 
-		aboutButton = createButton("About", y, new IButtonListener() {
+		aboutButton = createButton("About", y + 50f, new IButtonListener() {
 			public void onButtonTapped() {
 				ScreenManager.instance.switchTo(R.screens.about, false);
 			}
 		});
 
-		hiScoresButton = createButton("Hi Scores", y - 90f, new IButtonListener() {
+		hiScoresButton = createButton("Hi Scores", y - 50f, new IButtonListener() {
 			public void onButtonTapped() {
 				ScreenManager.instance.switchTo(R.screens.hiscores, false);
 			}
@@ -79,45 +77,48 @@ public class ResultView implements IDrawable {
 		textbutton.deactivate();
 
 		setLocation(textbutton, f);
-		
+
 		return textbutton;
 	}
-	
+
 	private static void setLocation(TextButton btn, float y) {
 		btn.getLocation().set((Game.getVirtualWidth() - btn.getWidth()) / 2.0F, y);
 	}
 
 	public void activate() {
 		restartButton.activate();
-		shareScoreButton.activate();
+		// facebookButton.activate();
 		aboutButton.activate();
 		hiScoresButton.activate();
-		hideShareScoreButton = false;
-		if (!TurpClient.isRegistered() || TurpClient.getPlayer().getAuthProvider() == Player.AuthAnonymous)
-			shareScoreButton.setText(loginWithFacebook);
-		else
-			shareScoreButton.setText(shareScore);
-		setLocation(shareScoreButton, shareScoreButton.getLocation().y);
+		// hideShareScoreButton = false;
+		// if (!TurpClient.isRegistered() ||
+		// TurpClient.getPlayer().getAuthProvider() == Player.AuthAnonymous)
+		// facebookButton.setText(loginWithFacebook);
+		// else {
+		// facebookButton.setText(shareScore);
+		//
+		// }
+		// setLocation(facebookButton, facebookButton.getLocation().y);
 	}
 
 	public void deactivate() {
 		restartButton.deactivate();
-		shareScoreButton.deactivate();
+		// facebookButton.deactivate();
 		aboutButton.deactivate();
 		hiScoresButton.deactivate();
 	}
 
 	public void draw() {
 		restartButton.draw();
-		if (!hideShareScoreButton)
-			shareScoreButton.draw();
+		// if (!hideShareScoreButton)
+		// facebookButton.draw();
 		aboutButton.draw();
 		hiScoresButton.draw();
 		logo.draw();
 	}
 
 	public void hideShareScoreButton() {
-		shareScoreButton.deactivate();
-		hideShareScoreButton = true;
+		// facebookButton.deactivate();
+		// hideShareScoreButton = true;
 	}
 }
